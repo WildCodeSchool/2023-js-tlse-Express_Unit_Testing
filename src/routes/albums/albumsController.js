e852e2317827c1d15f8519e1526ce1cabc572057
almasyser <almasyser@wanadoo.fr>
2023-12-13 10:45:42 +0100

    database OK - developper le frontend afficher les valeurs cartes et listes

--- src/routes/albums/albumsController.js
+++ src/routes/albums/albumsController.js
@@ -2,27 +2,89 @@ const connexion = require('../../../db-config');
 const db = connexion.promise();
 
 const getAll = (req, res) => {
-  res.status(200).send('Get All route is OK');
+  db.query('SELECT * FROM albums')
+    .then(([result]) => {
+      if (result.length) {
+        res.status(200).json(result);
+      } else {
+        res.status(404);
+      }
+    })
+    .catch(res.status(500));
 };
 
 const getOne = (req, res) => {
-  res.status(200).send('Get One route is OK');
+  const { id } = req.params;
+  // const {id} =req.params.id; id de tout typr destructuré
+  db.query('SELECT * FROM albums WHERE id=?', [id])
+    .then(([result]) => {
+      if (result[0]) {
+        res.status(200).json(result);
+      } else {
+        res.status(404);
+      }
+    })
+    .catch(res.status(500));
 };
 
 const getTracksByAlbumId = (req, res) => {
-  res.status(200).send('Get Albums route is OK');
+  const { id } = req.params;
+  db.query('SELECT * FROM trackd WHERE id_album=?', [id]).then(([result]) => {
+    if (result[0]) {
+      res.status(200).json(result);
+    } else {
+      res.status(404);
+    }
+  });
 };
-
 const postAlbums = (req, res) => {
-  res.status(200).send('Post route is OK');
+  const { title, genre, picture, artist } = req.body;
+  db.query(
+    'INSERT INTO albums (title, genre, picture, artist) VALUES (?, ?, ?, ?)',
+    [title, genre, picture, artist]
+  )
+    .then(([result]) => {
+      if (result.affectedRows) {
+        res.status(201).json({
+          title: title,
+          genre: genre,
+          picture: picture,
+          artist: artist,
+          id: result.insertId,
+        });
+      }
+    })
+    .catch(res.status(500));
 };
 
 const updateAlbums = (req, res) => {
-  res.status(200).send('Update route is OK');
+  const { id } = req.params;
+  const { title, genre, picture, artist } = req.body;
+  db.query(
+    'UPDATE albums SET title=?, genre=?, picture=?, artist=? WHERE id=?',
+    [title, genre, picture, artist, id]
+  )
+    .then(([result]) => {
+      if (result.affectedRows) {
+        res.sendStatus(204);
+      } else {
+        res.status(404);
+      }
+    })
+    .catch(res.status(500));
 };
 
 const deleteAlbums = (req, res) => {
-  res.status(200).send('Delete route is Ok');
+  const id = parseint(req.params.id);
+  db.query('DELETE FROM albums WHERE id=?', [id])
+    .then(([result]) => {
+      if (result.affectedRows) {
+        res.sendStatus(204);
+      } else {
+        res.status(404);
+      }
+    })
+    .catch(res.status(500));
 };
 
 module.exports = {
--- src/routes/tracks/tracksController.js
+++ src/routes/tracks/tracksController.js
@@ -1,24 +1,87 @@
 const connexion = require('../../../db-config');
 const db = connexion.promise();
 
-const getOne = (req, res) => {
-  res.status(200).send('Get One route is OK');
-};
-
 const getAll = (req, res) => {
-  res.status(200).send('Get All route is OK');
+  db.query('SELECT * FROM track')
+    .then(([result]) => {
+      if (result.length) {
+        res.status(200).json(result);
+      } else {
+        res.status(404);
+      }
+    })
+    .catch(res.status(500));
 };
 
+const getOne = (req, res) => {
+  const { id } = req.params;
+  db.query('SELECT * FROM track WHERE id=?', [id])
+    .then(([result]) => {
+      if (result[0]) {
+        res.status(200).json(result);
+      } else {
+        res.status(404);
+      }
+    })
+    .catch(res.status(500));
+};
+
+const getTracksByAlbumId = (req, res) => {
+  const { id } = req.params;
+  db.query('SELECT * FROM track WHERE id_album=?', [id]).then(([result]) => {
+    if (result[0]) {
+      res.status(200).json(result);
+    } else {
+      res.status(404);
+    }
+  });
+};
 const postTracks = (req, res) => {
-  res.status(200).send('Post route is OK');
+  const { title, genre, picture, artist } = req.body;
+  db.query(
+    'INSERT INTO albums (title, youtube_url, id_album) VALUES (?, ?, ?)',
+    [title, genre, picture, artist]
+  )
+    .then(([result]) => {
+      if (result.affectedRows) {
+        res.status(201).json({
+          title: title,
+          youtube_url: youtube_url,
+          id: result.insertId,
+        });
+      }
+    })
+    .catch(res.status(500));
 };
 
 const updateTracks = (req, res) => {
-  res.status(200).send('Update route is OK');
+  const { id } = req.params;
+  const { title, genre, picture, artist } = req.body;
+  db.query(
+    'UPDATE albums SET title=?, genre=?, picture=?, artist=? WHERE id=?',
+    [title, genre, picture, artist, id]
+  )
+    .then(([result]) => {
+      if (result.affectedRows) {
+        res.sendStatus(204);
+      } else {
+        res.status(404);
+      }
+    })
+    .catch(res.status(500));
 };
 
 const deleteTracks = (req, res) => {
-  res.status(200).send('Delete route is OK');
+  const id = parseint(req.params.id);
+  db.query('DELETE FROM albums WHERE id=?', [id])
+    .then(([result]) => {
+      if (result.affectedRows) {
+        res.sendStatus(204);
+      } else {
+        res.status(404);
+      }
+    })
+    .catch(res.status(500));
 };
 
 module.exports = { getOne, getAll, postTracks, updateTracks, deleteTracks };
